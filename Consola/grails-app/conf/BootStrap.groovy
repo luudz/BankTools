a572ac351b6c49e29f233d7f050de35da20268aa
JessicaMatias <iscmatias@outlook.com>
2015-03-12 10:55:36 -0600

    Agregue mas registros

--- Consola/grails-app/conf/BootStrap.groovy
+++ Consola/grails-app/conf/BootStrap.groovy
@@ -3,7 +3,11 @@ class BootStrap {
 
     def init = { servletContext ->
     	if(!CatalogoVariables.count()){
-    		new CatalogoVariables(nombre:"Regla001", descripcion:"Esta es la regla Regla001",enabled:").save(failonError:true);
+    		new CatalogoVariables(nombre:"Regla001", descripcion:"Esta es la regla Regla001",enabled:true).save(failonError:true);
+    		new CatalogoVariables(nombre:"Regla002", descripcion:"Esta es la regla Regla002",enabled:false).save(failonError:true);
+    		new CatalogoVariables(nombre:"Regla003", descripcion:"Esta es la regla Regla003",enabled:true).save(failonError:true);
+    		new CatalogoVariables(nombre:"Regla004", descripcion:"Esta es la regla Regla004",enabled:false).save(failonError:true);
+    		new CatalogoVariables(nombre:"Regla005", descripcion:"Esta es la regla Regla005",enabled:true).save(failonError:true);
     		
     	}
     }
