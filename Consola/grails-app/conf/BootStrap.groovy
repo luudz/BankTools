e2ed0750baf25b524b7f421f6940b181b174a134
JessicaMatias <iscmatias@outlook.com>
2015-03-12 10:47:11 -0600

    Agregue un registro en el bootStrap

--- Consola/grails-app/conf/BootStrap.groovy
+++ Consola/grails-app/conf/BootStrap.groovy
@@ -1,6 +1,11 @@
+import com.mx.eglobal.CatalogoVariables;
 class BootStrap {
 
     def init = { servletContext ->
+    	if(!CatalogoVariables.count()){
+    		new CatalogoVariables(nombre:"Regla001", descripcion:"Esta es la regla Regla001",enabled:").save(failonError:true);
+    		
+    	}
     }
     def destroy = {
     }
